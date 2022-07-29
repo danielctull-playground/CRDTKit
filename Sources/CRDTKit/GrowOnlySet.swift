d6f232fa5499f86e736bdcbd55172fccba6aedf5
Daniel Tull <dt@danieltull.co.uk>
2022-07-29 11:10:46 +0100

    Conform GrowOnlySet to Collection

--- Sources/CRDTKit/GrowOnlySet.swift
+++ Sources/CRDTKit/GrowOnlySet.swift
@@ -1,6 +1,7 @@
 
 public struct GrowOnlySet<Element: Hashable>: Equatable {
-    public private(set) var elements: Set<Element>
+
+    private var elements: Set<Element>
 
     public init(_ elements: Set<Element> = []) {
         self.elements = elements
@@ -20,6 +21,33 @@ extension GrowOnlySet {
 
     public mutating func formUnion(_ other: GrowOnlySet) {
         elements.formUnion(other.elements)
+    }
+}
+
+// MARK: - Collection
+
+extension GrowOnlySet: Collection {
+
+    public struct Index: Comparable {
+        fileprivate let index: Set<Element>.Index
+        fileprivate init(_ index: Set<Element>.Index) {
+            self.index = index
+        }
+        public static func < (lhs: Self, rhs: Self) -> Bool {
+            lhs.index < rhs.index
+        }
+    }
+
+    public var startIndex: Index { Index(elements.startIndex) }
+
+    public var endIndex: Index { Index(elements.endIndex) }
+
+    public func index(after i: Index) -> Index {
+        Index(elements.index(after: i.index))
+    }
+
+    public subscript(position: Index) -> Element {
+        elements[position.index]
     }
 }
 
--- Tests/CRDTKitTests/GrowOnlySetTests.swift
+++ Tests/CRDTKitTests/GrowOnlySetTests.swift
@@ -16,17 +16,18 @@ final class GrowOnlySetTests: XCTestCase {
     func testInit() {
         let value = Int.random
         let set = GrowOnlySet([value])
-        XCTAssertEqual(set.elements, [value])
+        XCTAssert(set.contains(value))
     }
 
     func testInsert() {
         var set = GrowOnlySet<Int>()
         let value1 = Int.random
         set.insert(value1)
-        XCTAssertEqual(set.elements, [value1])
+        XCTAssert(set.contains(value1))
         let value2 = Int.random
         set.insert(value2)
-        XCTAssertEqual(set.elements, [value1, value2])
+        XCTAssert(set.contains(value1))
+        XCTAssert(set.contains(value2))
     }
 
     func testFormUnion() {
@@ -35,7 +36,8 @@ final class GrowOnlySetTests: XCTestCase {
         var set1 = GrowOnlySet([value1])
         let set2 = GrowOnlySet([value2])
         set1.formUnion(set2)
-        XCTAssertEqual(set1.elements, [value1, value2])
+        XCTAssert(set1.contains(value1))
+        XCTAssert(set1.contains(value2))
     }
 
     func testLaws() {
