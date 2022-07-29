1be83e1c38f5a9981ed2ec0a8a303e39ab472d43
Daniel Tull <dt@danieltull.co.uk>
2022-07-29 13:09:10 +0100

    Use the fancy new Swift 5.7 syntax

--- Sources/CRDTKit/GrowOnlySet.swift
+++ Sources/CRDTKit/GrowOnlySet.swift
@@ -9,7 +9,7 @@ extension GrowOnlySet {
         self.init(elements: [])
     }
 
-    public init<S: Sequence>(_ sequence: S) where S.Element == Element {
+    public init(_ sequence: some Sequence<Element>) {
         self.init(elements: Set(sequence))
     }
 }
